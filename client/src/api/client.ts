09896cbd3a1d2c14512d62be201d8b6b9b44c971
kuzyanin <kuzyanin@users.noreply.github.com>
2026-07-17 01:04:42 +0400

    Fix API URL: use relative path instead of localhost

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Page, NewsItem, Photo, Video, ChurchEvent, Announcement, SiteSetting, LoginResponse } from './types';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = '/api';
 
 const api = axios.create({
   baseURL: API_BASE,
